9334f6e693551f6898f5ad676337797dff161bfa
wutaiy0129 <stony870129@gmai.com>
2024-08-19 13:28:40 +0800

    PWA fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f4a68738'], (function (workbox) { 'use strict';
     "revision": "f5c892d964fbfc319f7e5ea4a6015849"
   }, {
     "url": "index.html",
-    "revision": "0.kiqrjjdus28"
+    "revision": "0.ifbdi0de7l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Tony-official-main/sw.js', { scope: '/Tony-official-main/' })})}
